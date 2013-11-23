f59397a4b874e9fa38007946a192d6a5b5c7af68
Bernat Arlandis <berarma@hotmail.com>
2013-11-23 17:48:56 +0100

    Fixed code formatting

--- jquery.freeTheCookies.js
+++ jquery.freeTheCookies.js
@@ -9,143 +9,143 @@
 "use strict";
 (function($, window, undefined) {
 
-    var
-    $window = $( window ),
-    initialized = false,
-    $block = $( "<div/>" ),
-    $close = $( "<div/>" ),
-    defaults =  {
-    	cookieName: "cookie-consent",
-    	cookieOptions: {
-    		expires: 365,
-    		path: "/"
-    	},
-    	speed: 300,
-    	blockHtml: "This site needs <em>cookies</em> to offer its services. By using our services you're accepting the use we make of <em>cookies</em>. <a href='/cookie_policy'>More information</a>.",
-    	blockAttrs: {
-    	},
-    	blockCss: {
-    		padding: "0.5em",
-    		border: "1px solid #ccc",
-    		borderStyle: "solid none",
-    		textAlign: "center",
-    		font: "normal 0.9em/1.3 sans-serif",
-    		backgroundColor: "#ffe8a0",
-    		color: "#333",
-    	},
-    	closeHtml: "X",
-    	closeAttrs: {
-    		title: 'Close',
-    	},
-    	closeCss: {
-    		"float": "right",
-    		marginLeft: "1em",
-    		fontWeight: "bold",
-    		cursor: "pointer"
-    	},
-    	onUserAction: function( e ) {
-    		return true;
-    	}
-    },
-    settings,
-    $events = $( defaults ),
-    self = $.freeTheCookies = function( options ) {
+	var
+	$window = $( window ),
+	initialized = false,
+	$block = $( "<div/>" ),
+	$close = $( "<div/>" ),
+	defaults =  {
+		cookieName: "cookie-consent",
+		cookieOptions: {
+			expires: 365,
+			path: "/"
+		},
+		speed: 300,
+		blockHtml: "This site needs <em>cookies</em> to offer its services. By using our services you're accepting the use we make of <em>cookies</em>. <a href='/cookie_policy'>More information</a>.",
+		blockAttrs: {
+		},
+		blockCss: {
+			padding: "0.5em",
+			border: "1px solid #ccc",
+			borderStyle: "solid none",
+			textAlign: "center",
+			font: "normal 0.9em/1.3 sans-serif",
+			backgroundColor: "#ffe8a0",
+			color: "#333",
+		},
+		closeHtml: "X",
+		closeAttrs: {
+			title: 'Close',
+		},
+		closeCss: {
+			"float": "right",
+			marginLeft: "1em",
+			fontWeight: "bold",
+			cursor: "pointer"
+		},
+		onUserAction: function( e ) {
+			return true;
+		}
+	},
+	settings,
+	$events = $( defaults ),
+	self = $.freeTheCookies = function( options ) {
 
-      var callback = null;
+		var callback = null;
 
-      if ( options !== undefined ) {
-        if ( $.isFunction( options ) ) {
-          callback = options;
-          options = {};
-        }
-        if ( options.onConsent ) {
-          callback = options.onConsent;
-          delete options.onConsent;
-        }
-      }
+		if ( options !== undefined ) {
+			if ( $.isFunction( options ) ) {
+				callback = options;
+				options = {};
+			}
+			if ( options.onConsent ) {
+				callback = options.onConsent;
+				delete options.onConsent;
+			}
+		}
 
-      if ( !initialized ) {
-        settings = $.extend( {}, defaults, options );
-      }
+		if ( !initialized ) {
+			settings = $.extend( {}, defaults, options );
+		}
 
-      if ( callback ) {
-        $events.bind( "onConsent", callback);
-      }
+		if ( callback ) {
+			$events.bind( "onConsent", callback);
+		}
 
-      if ( $.cookie( settings.cookieName ) ) {
-        if ( !initialized ) {
-          $events.triggerHandler( "onConsent" );
-        } else {
-          if ( callback ) {
-            callback();
-          }
-        }
-      }
+		if ( $.cookie( settings.cookieName ) ) {
+			if ( !initialized ) {
+				$events.triggerHandler( "onConsent" );
+			} else {
+				if ( callback ) {
+					callback();
+				}
+			}
+		}
 
-      if ( $.cookie( settings.cookieName ) ) {
-        initialized = true;
-      	return true;
-      }
+		if ( $.cookie( settings.cookieName ) ) {
+			initialized = true;
+			return true;
+		}
 
-      if ( !initialized ) {
+		if ( !initialized ) {
 
-        $block.attr( settings.blockAttrs ).html( settings.blockHtml ).css( settings.blockCss );
-        $close.attr( settings.closeAttrs ).html( settings.closeHtml ).css( settings.closeCss );
-        $close.prependTo( $block );
-        $block.prependTo( "body" );
-        $block.hide();
-        $block.slideDown( settings.speed, function() {
+			$block.attr( settings.blockAttrs ).html( settings.blockHtml ).css( settings.blockCss );
+			$close.attr( settings.closeAttrs ).html( settings.closeHtml ).css( settings.closeCss );
+			$close.prependTo( $block );
+			$block.prependTo( "body" );
+			$block.hide();
+			$block.slideDown( settings.speed, function() {
 
-            window.scrollTo( 0, 0 );
+				window.scrollTo( 0, 0 );
 
-            $block.bind( "click", function( e ) {
-                e.stopPropagation();
-            });
+				$block.bind( "click", function( e ) {
+					e.stopPropagation();
+				});
 
-            $close.bind( "click", function() {
-                setCookiesFree();
-            });
+				$close.bind( "click", function() {
+					setCookiesFree();
+				});
 
-            $( "body" ).bind( "click.cookie-consent", function( e ) {
-                userAction( e );
-            });
+				$( "body" ).bind( "click.cookie-consent", function( e ) {
+					userAction( e );
+				});
 
-            var blockHeight = $block.height();
-            $window.bind( 'scroll.cookie-consent', function( e ) {
-                if ( window.pageYOffset >= blockHeight ) {
-                  userAction( e );
-                  $window.unbind( ".cookie-consent" );
-                }
-            });
+				var blockHeight = $block.height();
+				$window.bind( 'scroll.cookie-consent', function( e ) {
+					if ( window.pageYOffset >= blockHeight ) {
+						userAction( e );
+						$window.unbind( ".cookie-consent" );
+					}
+				});
 
-        });
+			});
 
-      }
+		}
 
-      initialized = true;
+		initialized = true;
 
-      return false;
+		return false;
 
-    };
+	};
 
-    function setCookiesFree() {
-      $window.unbind( ".cookie-consent" );
-      $( "body" ).unbind( ".cookie-consent" );
-      $.cookie( settings.cookieName, "1", settings.cookieOptions);
-      $block.slideUp( settings.speed );
-      $events.triggerHandler( "onConsent" );
-    }
+	function setCookiesFree() {
+		$window.unbind( ".cookie-consent" );
+		$( "body" ).unbind( ".cookie-consent" );
+		$.cookie( settings.cookieName, "1", settings.cookieOptions);
+		$block.slideUp( settings.speed );
+		$events.triggerHandler( "onConsent" );
+	}
 
-    function userAction( e ) {
-      if ( settings.onUserAction( e ) ) {
-        setCookiesFree();
-        return true;
-      }
-      return false;
-    }
+	function userAction( e ) {
+		if ( settings.onUserAction( e ) ) {
+			setCookiesFree();
+			return true;
+		}
+		return false;
+	}
 
-    self.now = setCookiesFree;
+	self.now = setCookiesFree;
 
-    self.settings = defaults;
+	self.settings = defaults;
 
 })(jQuery, window);
